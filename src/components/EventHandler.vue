187326ffe3f90f91e25664ce6d2960dafb90be5a
athirarenjan <athirarenjan8@gmail.com>
2018-11-19 11:27:37 +0530

    Initial commit

---
+++ src/components/EventHandler.vue
@@ -0,0 +1,8 @@
+<template>
+  <div><h2>Event Handler Header</h2></div>
+</template>
+<script>
+export default {
+  name: "EventHandler"
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template v-if="show">
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Learning Links</h2>
+    <ul>
+      <li>
+        <a href="javascript:;" target="_blank" v-on:click="toggle">
+          Event Handling
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<template v-else> </template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Learning Vue.js",
+      show: true
+    };
+  },
+  methods: {
+    toggle: function() {
+      this.show = !this.show;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
